d6ea8fd5ce17b53d5dd98ff806c6a5dade018108
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2022-10-05 21:44:31 +0200

    First commit on master

---
+++ src/main/calculations/BasicMath.java
@@ -0,0 +1,12 @@
+package main.calculations;
+
+public class BasicMath {
+
+	public static void sum(int number1, int number2) {
+		//TODO - Do a sum of two numbers
+	}
+	
+	public static void substraction(int number1, int number2) {
+		//TODO - A substraction between two numbers
+	}
+}
