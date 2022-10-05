fe63acbbd0922391e28e200d83ba70abe37eb955
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2022-10-05 21:52:29 +0200

    Adding javadoc to sum and substraction

--- src/main/calculations/BasicMath.java
+++ src/main/calculations/BasicMath.java
@@ -2,10 +2,22 @@ package main.calculations;
 
 public class BasicMath {
 
+	/**
+	 * Sums the two given numbers and prints the result in console
+	 * 
+	 * @param number1 A number to be summed
+	 * @param number2 A number to be summed
+	 */
 	public static void sum(int number1, int number2) {
 		System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
 	}
 	
+	/**
+	 * Substracts the second given number, to the first one
+	 * 
+	 * @param number1 The number that will have substraction applied
+	 * @param number2 The number that will be substracted to the first one
+	 */
 	public static void substraction(int number1, int number2) {
 		System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
 	}
