613057f12357769197ce98c27f5b943903d7c61e
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2022-10-05 22:31:23 +0200

    power implemented, and tested.

--- src/main/AppStart.java
+++ src/main/AppStart.java
@@ -1,10 +1,14 @@
 package main;
 
+import main.calculations.BasicMath;
+
 public class AppStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Starting the app...");
+		
+		BasicMath.power(2, 5);
 	}
 
 }
--- src/main/calculations/BasicMath.java
+++ src/main/calculations/BasicMath.java
@@ -50,5 +50,14 @@ public class BasicMath {
 	 */
 	public static void power(int base, int exponent) {
 		
+		int result = 1;
+		
+		for(int i = 0; i < exponent; i++)
+		{
+			result *= base;
+		}
+		
+		System.out.println(base + " ^ " + exponent + " = " + result);
+		
 	}
 }
